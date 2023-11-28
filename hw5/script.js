cab61ad50c6c720c743f85ffbf5ae939dc021ee9
Sergey Nagornykh <serega1905@ukr.net>
2023-11-28 23:13:27 +0200

    add hw5(selectors/darkmode)

---
+++ hw5/script.js
@@ -0,0 +1,66 @@
+// base set up
+let isEnableDarkMode = false;
+const toggleBtn = document.querySelector('#toggle-btn');
+const description = document.querySelector('.description');
+
+// handle enable/disable dark mode by click on button
+toggleBtn.addEventListener('click', () => {
+  if(isEnableDarkMode) {
+    turnOff();
+  } else {
+    turnOn();
+  }
+
+  isEnableDarkMode = !isEnableDarkMode;
+});
+
+// load last saved mode from localStorage
+loadDarkMode();
+
+function turnOn() {
+  toggleBtn.innerHTML = 'Turn on';
+  description.innerHTML = `Last turn off: ${formatDate(new Date())}`;
+  document.body.classList.add('dark-theme');
+  localStorage.setItem('dark-mode', 'true');
+}
+
+function turnOff() {
+  toggleBtn.innerHTML = 'Turn off';
+  description.innerHTML = `Last turn on: ${formatDate(new Date())}`
+  document.body.classList.remove('dark-theme');
+  localStorage.setItem('dark-mode', 'false');
+}
+
+function loadDarkMode() {
+  if(localStorage.getItem('dark-mode') === 'true') {
+    turnOn();
+    isEnableDarkMode = true;
+  } else {
+    turnOff();
+    isEnableDarkMode = false;
+  }
+}
+
+
+/**
+ * Dates snippet https://bobbyhadz.com/blog/javascript-format-date-yyyy-mm-dd-hh-mm-ss
+ */
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function formatDate(date) {
+  return (
+    [
+      padTo2Digits(date.getDate()),
+      padTo2Digits(date.getMonth() + 1),
+      date.getFullYear()
+    ].join('-') +
+    ' ' +
+    [
+      padTo2Digits(date.getHours()),
+      padTo2Digits(date.getMinutes()),
+      padTo2Digits(date.getSeconds()),
+    ].join(':')
+  );
+}
---
+++ hw5/selectors.js
@@ -0,0 +1,10 @@
+const h2 = document.body.lastElementChild.firstElementChild;
+const h2ById = document.getElementById('headerTwo');
+const h2ByQuerySelector = document.querySelector('#headerTwo');
+
+
+const firstSection = document.querySelector('section.firstSection')
+
+const li = document.querySelector('ul li:nth-of-type(5)');
+
+const div = document.getElementsByClassName('hatredLevelBlock')[0];
