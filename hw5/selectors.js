bc8ba4e58b34f5b339426e6a849cf954f75a0476
Sergey Nagornykh <serega1905@ukr.net>
2023-12-19 22:02:24 +0200

    add hw5(selectors/darkmode) (#5)
    * add hw5(selectors/darkmode)
    
    * fix minor issues

---
+++ hw5/selectors.js
@@ -0,0 +1,10 @@
+const h2 = document.body.lastElementChild.firstElementChild;
+const h2ById = document.getElementById('headerTwo');
+const h2ByQuerySelector = document.querySelector('#headerTwo');
+
+
+const firstSection = document.querySelector('section.firstSection')
+
+const li = document.getElementsByTagName('ul')[0].lastElementChild.previousElementSibling;
+
+const div = document.getElementsByClassName('hatredLevelBlock')[0];
